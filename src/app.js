e896bcc5b772b0cceb10a97e64c9e86e85180df3
amitmahata <00.amitmahata37@gmail.com>
2025-05-19 23:25:22 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -10,6 +10,25 @@ app.use("/express", (req,res) => {
     res.send('Hello from Express!');
 })
 
-app.listen(3000, () => {
-    console.log('Server is successfully listening on port 3000...');
+app.get("/user", (req, res, next) => {
+    console.log(req.query);
+    console.log(req.params);
+    console.log("First Response");
+    //res.send({firstName: "Amit", lastName: "Mahata"});
+    next();
+},
+    (req, res, next) => {
+        console.log("Second Response");
+        //res.send({ firstName: "Amit", lastName: "Mahata" });
+        next();
+    },
+    (req, res, next) => {
+        console.log("Third Response");
+        res.send({ firstName: "Amit", lastName: "Mahata" });
+        //next();
+    }
+);
+
+app.listen(2222, () => {
+    console.log('Server is successfully listening on port 2222...');
 });
