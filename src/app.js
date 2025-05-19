9daa7654cddaf1a2a0c3930ce9d6e0783a3bb13d
amitmahata <00.amitmahata37@gmail.com>
2025-05-19 22:01:09 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use("/hello", (req,res) => {
+    res.send('Hello devs!');
+})
+
+app.use("/express", (req,res) => {
+    res.send('Hello from Express!');
+})
+
+app.listen(3000, () => {
+    console.log('Server is successfully listening on port 3000...');
+});
