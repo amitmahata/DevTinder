b1e25ca3dfcb13677eaaba4e2b8521dcef1aca7c
amitmahata <00.amitmahata37@gmail.com>
2025-05-22 00:37:19 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -18,6 +18,34 @@ app.post("/signup", async (req, res) => {
     }
 });
 
+// Get user by emailId
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try {
+        const users = await User.find({ emailId: userEmail });
+        if (users.length === 0) {
+            return res.status(404).send("User not found...");
+        }
+        res.send(users);
+    }
+    catch (err) {
+        res.status(500).send("Error while fetching user..." + err.message);
+    }
+});
+
+// Get all users to feed the UI
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send("Error while fetching users..." + err.message);
+    }
+});
+
 
 connectDB().then(() => {
     console.log("MongoDB is successfully connected...");
